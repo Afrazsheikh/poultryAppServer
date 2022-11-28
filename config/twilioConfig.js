2e0b0117e6fd091e3f2ce20b718e074bc048b9a2
Afraz <79227256+Afrazsheikh@users.noreply.github.com>
2022-11-28 10:42:07 +0530

    Update twilioConfig.js

--- config/twilioConfig.js
+++ config/twilioConfig.js
@@ -1,8 +1,8 @@
 const twilioConfig = {
-    serviceID: 'MGbd1864d591ad92b640980d94fbda9348',
-    accountSID: 'ACb38fe2619b6dc23c726f584745581e83',
-    authToken: '83f84e865d052304614f5bf2e30d5f83',
-    serviceSID: 'VA131315d1fb67799e974483a90cd024f1'  
+    serviceID: sndkjfhn
+    accountSID: serfkwkesru
+    authToken: sdjhrfkhsd
+    serviceSID:  djkrfgkh
 }
 
 module.exports = twilioConfig
